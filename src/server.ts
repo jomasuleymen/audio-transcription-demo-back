c9e8c36e7795398e99226e1662abe4b3adaa3696
jomasuleymen <joma.suleymen@gmail.com>
2025-08-29 12:00:10 +0500

    integrate apollo server, utilizing @graphql-tools for type definitions and resolver merging

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@as-integrations/express5';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
 import { env } from '@shared/utils/envConfig';
 import cors from 'cors';
 import express, { type Express } from 'express';
@@ -10,5 +14,16 @@ appServer.use(
 	})
 );
 appServer.use(express.json());
+
+const typeDefs = mergeTypeDefs(loadFilesSync('src/**/*.{graphql,gql}'));
+const resolvers = mergeResolvers(loadFilesSync('src/**/*.resolver.ts'));
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+await server.start();
+
+appServer.use('/graphql', expressMiddleware(server));
 
 export { appServer };
